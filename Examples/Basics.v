eec3d31fe89ac4a7036d39ae012990856f4dff65
sergio <thesergio.gomes@gmail.com>
2025-01-11 16:43:13 -0300

    LG: fixing example.

--- Examples/Basics.v
+++ Examples/Basics.v
@@ -11,17 +11,16 @@ Inductive day : Type :=
  | sunday.
 
 
-Definition next_weekday (d:day) : day :=
+ Definition next_weekday (d:day) : day :=
   match d with
-  | monday -> tuesday
-  | tuesday -> wednesday
-  | wednesday -> thursday
-  | thursday -> friday
-  | friday -> monday
-  | saturday -> monday
-  | sunday -> monday
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
   end.
-
 
 Compute (next_weekday friday).
 Compute (next_weekday (next_weekday saturday)).
