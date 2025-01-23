15f17974fd2090ee697e21065ea7462bd6ed41ac
sergio <thesergio.gomes@gmail.com>
2025-01-22 22:29:05 -0300

    LF: exercise 4

---
+++ Exercises/exerc4.v
@@ -0,0 +1,23 @@
+From Coq Require Export String.
+
+
+Fixpoint leb (n m : nat) : bool :=
+  match n with
+  | O => true
+  | S n' =>
+      match m with
+      | O => false
+      | S m' => leb n' m'
+      end
+  end.
+
+(*Tests with natural numbers for less-than*)
+Definition ltb (n m : nat) : bool :=  leb (S n) m.
+
+Notation "x <? y" := (ltb x y) (at level 70) : nat_scope.
+Example test_ltb1: (ltb 2 2) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_ltb2: (ltb 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_ltb3: (ltb 4 2) = false.
+Proof. simpl. reflexivity. Qed.
